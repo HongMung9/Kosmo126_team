98c00232fed562d27d2562e95c00182a721dbc77
ttw1004h <ttw1004h@naver.com>
2023-04-07 10:18:55 +0900

    부트

---
+++ src/main/java/com/example/springBoot_basic/KosmoTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBoot_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosmoTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosmoTeamApplication.class, args);
+	}
+
+}
